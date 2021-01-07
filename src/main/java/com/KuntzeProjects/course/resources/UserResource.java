9a11a521e52bcd519c0fce3f447388c61b7bcade
Hassan <hskuntze@gmail.com>
2021-01-07 15:54:37 -0300

    Service Layer, registro de componente

--- src/main/java/com/KuntzeProjects/course/resources/UserResource.java
+++ src/main/java/com/KuntzeProjects/course/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.KuntzeProjects.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.KuntzeProjects.course.entities.User;
+import com.KuntzeProjects.course.services.UserService;
 
 //Indica que é um recurso web controlado por um controlador rest
 @RestController
 @RequestMapping(value = "/users") //Dá um nome ao recurso
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	//ResponseEntity é um tipo específico do Spring pra retornar respostas de requisições web
 	@GetMapping //Indica que esse método responde ao método 'GET' do HTTP
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "João", "joão@gmail.com", "6198876543", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}") //indica que a requisição 'GET' aceitará um determinado {id} dentro da URL
+	public ResponseEntity<User> findById(@PathVariable Long id){ //para que utilizemos o {id}, primeiro o Spring deve reconhecê-lo como tal, para isso, utilizando a anotação "PathVariable"
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
