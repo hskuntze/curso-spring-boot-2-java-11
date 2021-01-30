a1992e7126d3214c82305cb15175f79791154f77
Hassan <hskuntze@gmail.com>
2021-01-30 18:33:34 -0300

    Exception Handler: findById()

--- src/main/java/com/KuntzeProjects/course/services/UserService.java
+++ src/main/java/com/KuntzeProjects/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.KuntzeProjects.course.entities.User;
 import com.KuntzeProjects.course.repositories.UserRepository;
+import com.KuntzeProjects.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,10 +22,26 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id); //findById retorna o tipo "Optional" existente desde o Java8
-		return obj.get(); //a função .get do "Optional" retorna o tipo que está definido no Optional, no caso, User
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
 		return repository.save(obj); //Esta operação retorna um objeto "salvo" (realiza a operação de salvamento)
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id); //'getOne' permite que o objeto seja monitorado pelo JPA antes de ser modificado definitivamente no banco de dados
+		updateData(entity, obj); //Método auxiliar
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
